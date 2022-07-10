a8ddac7a8cdeb21ecb9dc9e81d2870d8c3b15d32
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-07-10 16:42:18 +0300

    Update FlxHitbox.hx

--- source/android/flixel/FlxHitbox.hx
+++ source/android/flixel/FlxHitbox.hx
@@ -41,53 +41,41 @@ class FlxHitbox extends FlxSpriteGroup
 		scrollFactor.set();
 
 		hitbox = new FlxSpriteGroup();
-		hitbox.scrollFactor.set();
-
 		hitbox.add(add(buttonLeft = createHitbox(0, 0, 'left', 0xFFFF00FF)));
 		hitbox.add(add(buttonDown = createHitbox(FlxG.width / 4, 0, 'down', 0xFF00FFFF)));
 		hitbox.add(add(buttonUp = createHitbox(FlxG.width / 2, 0, 'up', 0xFF00FF00)));
 		hitbox.add(add(buttonRight = createHitbox((FlxG.width / 2) + (FlxG.width / 4), 0, 'right', 0xFFFF0000)));
+		hitbox.scrollFactor.set();
 	}
 
 	override function destroy()
 	{
 		super.destroy();
 
-		if (hitbox != null)
-		{
-			hitbox = FlxDestroyUtil.destroy(hitbox);
-			hitbox = null;
-		}
-
-		if (buttonLeft != null)
-			buttonLeft = null;
-
-		if (buttonDown != null)
-			buttonDown = null;
-
-		if (buttonUp != null)
-			buttonUp = null;
-
-		if (buttonRight != null)
-			buttonRight = null;
+		hitbox = FlxDestroyUtil.destroy(hitbox);
+		hitbox = null;
+		buttonLeft = null;
+		buttonDown = null;
+		buttonUp = null;
+		buttonRight = null;
 	}
 
 	/**
 	 * @param   X          The x-position of the button.
 	 * @param   Y          The y-position of the button.
 	 * @param   Color      The color of the button.
-	 * @param   Callback   The callback for the button.
 	 * @return  The button
 	 */
-	public function createHitbox(X:Float, Y:Float, Graphic:String, ?Color:Int, ?OnClick:Void->Void):FlxButton
+	public function createHitbox(X:Float, Y:Float, Graphic:String, ?Color:Int = 0xFFFFFF):FlxButton
 	{
 		var button:FlxButton = new FlxButton(X, Y);
 		button.loadGraphic(FlxGraphic.fromFrame(FlxAtlasFrames.fromSparrow('assets/android/hitbox.png', 'assets/android/hitbox.xml').getByName(Graphic)));
 		button.setGraphicSize(Std.int(FlxG.width / 4), FlxG.height);
 		button.updateHitbox();
+		button.color = Color;
 		button.alpha = 0.00001;
 
-		var tween:FlxTween = null;
+		var tween:FlxTween;
 
 		button.onDown.callback = function()
 		{
@@ -97,6 +85,7 @@ class FlxHitbox extends FlxSpriteGroup
 			tween = FlxTween.num(button.alpha, 0.6, 0.06, {ease: FlxEase.circInOut}, function(value:Float)
 			{
 				button.alpha = value;
+				tween = null;
 			});
 		}
 
@@ -108,6 +97,7 @@ class FlxHitbox extends FlxSpriteGroup
 			tween = FlxTween.num(button.alpha, 0.00001, 0.15, {ease: FlxEase.circInOut}, function(value:Float)
 			{
 				button.alpha = value;
+				tween = null;
 			});
 		}
 
@@ -119,19 +109,12 @@ class FlxHitbox extends FlxSpriteGroup
 			tween = FlxTween.num(button.alpha, 0.00001, 0.15, {ease: FlxEase.circInOut}, function(value:Float)
 			{
 				button.alpha = value;
+				tween = null;
 			});
 		}
-
-		if (Color != null)
-			button.color = Color;
-
 		#if FLX_DEBUG
 		button.ignoreDrawDebug = true;
 		#end
-
-		if (OnClick != null)
-			button.onDown.callback = OnClick;
-
 		return button;
 	}
 }
